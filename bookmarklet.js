3958013eec8411215dd6e4f5e2691f0d5d9f36f6
jebibot <tjebibot@gmail.com>
2025-04-29 01:57:42 +0900

    feat: ogq emoticon

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,9 +1,7 @@
 (async () => {
-  const nAllViewer = document.getElementById("nAllViewer");
-
   const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-  const giftSubscription = async (szRecvId) => {
+  const gift = async (szRecvId, szOgqId) => {
     try {
       const options = {
         method: "GET",
@@ -12,76 +10,105 @@
         },
         credentials: "include",
       };
-
       const { nBroadNo, szBjId } = liveView.LiveViewInfo;
-      const body = new URLSearchParams({
-        szSendType: Math.random(),
-        szRecvId,
-        nBroadNo,
-        szSubscriptionBj: szBjId,
-      });
-      const createCode = await fetch(
-        `${ST_AFREECATV}/api/item/subscription.php?szWork=createCode`,
-        {
-          ...options,
-          method: "POST",
-          body,
-        }
-      );
-      if (!createCode.ok) {
-        return;
-      }
-      const code = await createCode.json();
-      if (code.RESULT !== 1 || !code.MSG) {
-        return;
-      }
+      const { szUserId, szUserChatId } = liveView.ChatInfo.myUserInfo;
 
-      const params = new URLSearchParams({
-        szWork: "sendItem",
-        szFileType: "json",
-        szData: code.MSG,
-        szRecvId,
-        szChatId: liveView.ChatInfo.myUserInfo.szUserChatId,
-        szSubscriptionBj: szBjId,
-        nBroadNo,
-        nItemType: "__ITEM_TYPE__",
-        broad_no: nBroadNo,
-        sys_type: "html5",
-        location: "live",
-      });
-      const sendItem = await fetch(
-        `${ST_AFREECATV}/api/item/subscription.php?${params.toString()}`,
-        options
-      );
-      if (!sendItem.ok) {
-        return;
-      }
-      const result = await sendItem.json();
-      if (result.RESULT !== 1 || result.FAIL_CNT || result.FAIL_LIST) {
-        return;
+      if (__ITEM_TYPE__ === "ogq") {
+        const body = new URLSearchParams({
+          mode: "sendGiftEmoticon",
+          broad_no: nBroadNo,
+          send_id: szUserId,
+          recieve_id: szRecvId,
+          ogq_id: szOgqId,
+        });
+        const sendGiftEmoticon = await fetch(
+          `${OGQ_MARKET_AFREECATV}/api.php`,
+          {
+            ...options,
+            method: "POST",
+            body,
+          }
+        );
+        const result = await sendGiftEmoticon.json();
+        if (!sendGiftEmoticon.ok || result.RESULT !== 1) {
+          return `선물 실패: ${szRecvId}: ${result.MSG || ""}`;
+        }
+      } else {
+        const body = new URLSearchParams({
+          szSendType: Math.random(),
+          szRecvId,
+          nBroadNo,
+          szSubscriptionBj: szBjId,
+        });
+        const createCode = await fetch(
+          `${ST_AFREECATV}/api/item/subscription.php?szWork=createCode`,
+          {
+            ...options,
+            method: "POST",
+            body,
+          }
+        );
+        const code = await createCode.json();
+        if (!createCode.ok || code.RESULT !== 1 || !code.MSG) {
+          return;
+        }
+
+        const params = new URLSearchParams({
+          szWork: "sendItem",
+          szFileType: "json",
+          szData: code.MSG,
+          szRecvId,
+          szChatId: szUserChatId,
+          szSubscriptionBj: szBjId,
+          nBroadNo,
+          nItemType: __ITEM_TYPE__,
+          broad_no: nBroadNo,
+          sys_type: "html5",
+          location: "live",
+        });
+        const sendItem = await fetch(
+          `${ST_AFREECATV}/api/item/subscription.php?${params.toString()}`,
+          options
+        );
+        const result = await sendItem.json();
+        if (
+          !sendItem.ok ||
+          result.RESULT !== 1 ||
+          result.FAIL_CNT ||
+          result.FAIL_LIST
+        ) {
+          return `선물 실패: ${szRecvId}: ${
+            result.MSG || result.FAIL_LIST?.[0].msg || ""
+          }`;
+        }
       }
       return `선물 성공: ${szRecvId}`;
-    } catch (e) {}
+    } catch {}
   };
 
   const getList = async (tries = 0) => {
-    if (tries % 8 === 0) {
-      liveView.playerController.sendChUser();
-      await wait(300);
+    if (tries >= 10) {
+      return;
     }
-    const listLayer = liveView.Chat.chatUserListLayer;
-    const chatCount = Object.values(listLayer.userListSeparatedByGrade).reduce(
-      (acc, cur) => acc + Object.keys(cur).length - 1,
-      0
-    );
-    const viewerCount = Number(nAllViewer.textContent.replace(/,/g, "")) || 5;
-    if (chatCount < 0.5 * viewerCount) {
-      if (tries > 100) {
-        return;
+    const { promise, resolve, reject } = Promise.withResolvers();
+    const { chatSocket } = livePlayer;
+    const originalReceived = chatSocket.received;
+    chatSocket.received = function (msg) {
+      originalReceived.call(this, msg);
+      if (msg.serviceCode === 110) {
+        resolve();
       }
-      return wait(100).then(() => getList(tries + 1));
+    };
+    setTimeout(reject, 1000);
+    try {
+      chatSocket.sender.requestUserListInChannel();
+      await promise;
+      return liveView.Chat.chatUserListLayer.getUserListForSDK();
+    } catch {
+      return getList(tries + 1);
+    } finally {
+      chatSocket.received = originalReceived;
     }
-    return listLayer.getUserListForSDK();
   };
 
   const getRandom = (arr, n) => {
@@ -96,50 +123,70 @@
     return result;
   };
 
-  liveView.Chat.showToastMsg?.("참여자 목록을 가져오는 중입니다.");
-  const userList = await getList();
-  if (userList == null) {
-    alert("참여자 목록을 가져오는데 실패했습니다.");
-    return;
-  }
-
-  const TYPE_MAP = {
-    매니저: "manager",
-    열혈팬: "hot",
-    구독팬: "subscription",
-    팬: "fan",
-    서포터: "supporter",
-    일반: "normal",
-  };
-  const types = prompt(
-    "선물할 대상을 콤마로 구분하여 입력해주세요.\n(매니저, 열혈팬, 구독팬, 팬, 서포터, 일반)"
-  );
-  const num = Number(prompt("선물할 인원을 입력해주세요."));
-  if (isNaN(num) || num < 1) {
-    alert("잘못된 인원입니다.");
-    return;
-  }
-  let list = [];
-  for (const t of types.split(",")) {
-    const l = userList[TYPE_MAP[t.trim()]];
-    if (l != null) {
-      list = list.concat(l);
+  const giftRandom = async (szOgqId) => {
+    liveView.Chat.showToastMsg?.("참여자 목록을 가져오는 중입니다.");
+    const userList = await getList();
+    if (userList == null) {
+      alert("참여자 목록을 가져오는데 실패했습니다.");
+      return;
     }
-  }
-  if (list.length === 0) {
-    alert("대상이 없습니다.");
-    return;
-  }
 
-  liveView.Chat.showToastMsg?.("선물 중입니다.");
-  const giftList = list.length <= num ? list : getRandom(list, num);
-  const giftSet = new Set(giftList.map((user) => user.id.split("(")[0]));
-  const result = [];
-  for (const user of giftSet) {
-    result.push((await giftSubscription(user)) || `선물 실패: ${user}`);
-    await wait(100);
+    const TYPE_MAP = {
+      매니저: "manager",
+      열혈팬: "hot",
+      구독팬: "subscription",
+      팬: "fan",
+      서포터: "supporter",
+      일반: "normal",
+    };
+    const types = prompt(
+      "선물할 대상을 콤마로 구분하여 입력해주세요.\n(매니저, 열혈팬, 구독팬, 팬, 서포터, 일반)"
+    );
+    const num = Number(prompt("선물할 인원을 입력해주세요."));
+    if (isNaN(num) || num < 1) {
+      alert("잘못된 인원입니다.");
+      return;
+    }
+    let list = [];
+    for (const t of types.split(",")) {
+      const l = userList[TYPE_MAP[t.trim()]];
+      if (l != null) {
+        list = list.concat(l);
+      }
+    }
+    if (list.length === 0) {
+      alert("대상이 없습니다.");
+      return;
+    }
+
+    liveView.Chat.showToastMsg?.("선물 중입니다.");
+    const giftList = list.length <= num ? list : getRandom(list, num);
+    const giftSet = new Set(giftList.map((user) => user.id.split("(")[0]));
+    const result = [];
+    for (const user of giftSet) {
+      result.push((await gift(user, szOgqId)) || `선물 실패: ${user}`);
+      await wait(100);
+    }
+    alert(result.join("\n"));
+  };
+
+  if (__ITEM_TYPE__ === "ogq") {
+    await liveView.Chat.showGiftOgqEmoticonLayer({
+      receiveId: "*",
+      receiveNick: "랜덤 선물",
+    });
+    const originalsendGift = window.sendGift;
+    window.sendGift = (szOgqId) => {
+      if ($("#recieve_id").val() !== "*") {
+        return originalsendGift(szOgqId);
+      }
+      giftRandom(szOgqId).catch(() => {
+        alert("오류가 발생했습니다.");
+      });
+    };
+  } else {
+    await giftRandom();
   }
-  alert(result.join("\n"));
 })().catch(() => {
   alert("오류가 발생했습니다.");
 });
--- build.js
+++ build.js
@@ -1,25 +1,26 @@
 const fs = require("fs");
-const { minify } = require("terser");
+const { minify_sync } = require("terser");
 
 (async () => {
   const code = fs.readFileSync("bookmarklet.js", "utf8");
-  const { code: minified } = await minify(code, {
-    compress: {
-      passes: 2,
-    },
-    format: {
-      ascii_only: true,
-      quote_style: 1,
-    },
-  });
-
   let html = fs.readFileSync("docs/index.html", "utf8");
   html = html.replace(
-    /data-type="(\d+)" href="javascript:(?:[^"]+)"/g,
-    (_, type) =>
-      `data-type="${type}" href="javascript:${encodeURI(
-        minified.replace("__ITEM_TYPE__", type)
-      )}"`
+    /data-type="(\w+)" href="javascript:(?:[^"]+)"/g,
+    (_, type) => {
+      const { code: minified } = minify_sync(code, {
+        compress: {
+          passes: 2,
+          global_defs: {
+            __ITEM_TYPE__: type,
+          },
+        },
+        format: {
+          ascii_only: true,
+          quote_style: 1,
+        },
+      });
+      return `data-type="${type}" href="javascript:${encodeURI(minified)}"`;
+    }
   );
   fs.writeFileSync("docs/index.html", html, "utf8");
 })();
